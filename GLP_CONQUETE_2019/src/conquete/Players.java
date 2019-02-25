0d0d247cc0ba14bc3f732f86dee8ba65b6d33132
bjm95 <eurasise@Gmail.com>
2019-02-25 19:27:08 +0100

    Update Players.java

--- GLP_CONQUETE_2019/src/conquete/Players.java
+++ GLP_CONQUETE_2019/src/conquete/Players.java
@@ -1,4 +1,6 @@
-package conquete;
+package data;
+
+import gui.elements.ItemsPanel;
 
 /**
  * @author macbook
@@ -8,20 +10,69 @@ public class Players{
 //Attributes
 	private String playerName;
 	private int lifePoint;
-	private Country playerCountry;
+	private String playerCountry;
 	private Players ennemy;
-	private Rounds round;
-	public Players() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Players(String playerName, int lifePoint, Country playerCountry, Players ennemy, Rounds round) {
-		super();
+	//private Rounds round;
+	private boolean play;
+	private ItemsPanel items;
+	private int id;
+	
+	public Players(String playerName, int lifePoint,Players ennemy, String playerCountry,boolean play,ItemsPanel items) {
 		this.playerName = playerName;
 		this.lifePoint = lifePoint;
 		this.playerCountry = playerCountry;
 		this.ennemy = ennemy; 
-		this.round = round;
+		//this.round = round;
+		this.play = play;
+		this.items = items;
 	}
+	
+	public String getPlayerName() {
+		return playerName;
+	}
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+	public int getLifePoint() {
+		return lifePoint;
+	}
+	public void setLifePoint(int lifePoint) {
+		this.lifePoint = lifePoint;
+	}
+	public String getPlayerCountry() {
+		return playerCountry;
+	}
+	public void setPlayerCountry(String playerCountry) {
+		this.playerCountry = playerCountry;
+		
+	}
+	public Players getEnnemy() {
+		return ennemy;
+	}
+	public void setEnnemy(Players ennemy) {
+		this.ennemy = ennemy;
+	}
+	public boolean isPlay() {
+		return play;
+	}
+	public void setPlay(boolean play) {
+		this.play = play;
+	}
+	public ItemsPanel getItems() {
+		return items;
+	}
+	public void setItems(ItemsPanel items) {
+		this.items = items;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public boolean getPlay() {
+		return play;
+	}
+	
 	
 }
